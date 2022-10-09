0447718f90cab35be696f65b31a60fa89a06bae3
NickdevC <nick_coles@msn.com>
2022-10-09 13:46:47 +0000

    Add initial commit and welcome message test

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def welcome_screen():
+    """
+    Displays the main screen for users on loading the game for the first time,
+    or when selecting to start a new game.
+    """
+    print("Got ships? Let's battle!")
+
+
+welcome_screen()
