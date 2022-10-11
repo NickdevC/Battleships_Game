1f7a6ae664a80859577a771ad3e2a7c7c0d1d62c
NickdevC <nick_coles@msn.com>
2022-10-11 15:23:42 +0000

    Add main title and imported time and sys modules

--- run.py
+++ run.py
@@ -1,13 +1,33 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Import libraries
+import time
+import sys
+
+def print_slow(str):
+    for letter in str:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(0.01)
+
 
 def welcome_screen():
     """
     Displays the main screen for users on loading the game for the first time,
     or when selecting to start a new game.
     """
-    print("Got ships? Let's battle!")
+    print("\033[34m Got ships? Let's battle!")
+    time.sleep(3)
+    print_slow("""\
+    \u001b[34m
+  ____        _   _   _           _     _           
+ |  _ \      | | | | | |         | |   (_)          
+ | |_) | __ _| |_| |_| | ___  ___| |__  _ _ __  ___ 
+ |  _ < / _` | __| __| |/ _ \/ __| '_ \| | '_ \/ __|
+ | |_) | (_| | |_| |_| |  __/\__ \ | | | | |_) \__ |
+ |____/ \__,_|\__|\__|_|\___||___/_| |_|_| .__/|___/
+                                         | |        
+                                         |_| 
+\u001b[0m       
+""")
 
 
 welcome_screen()
